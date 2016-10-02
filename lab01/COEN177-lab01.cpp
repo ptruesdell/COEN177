bfc02d9803e250ff7598b6ca5c844fa13d5e2baf
ptruesdell <parkertruesdell@gmail.com>
2016-10-02 16:18:00 -0700

    First commit

---
+++ lab01/COEN177-lab01.cpp
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/errno.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+	string command;
+
+	// Main loop
+	while (true) {
+
+		pid_t pid = 0;
+
+		// Print prompt
+		cout << "$> ";
+		std::cin >> command;
+
+		if (command == "exit") {
+			exit(0);
+		}
+		
+		else if (fork() != 0) { /* Fork off child process */
+			/* Parent Code */
+			wait(NULL);
+		} else {
+			int status;
+			//execl(command, parameters, 0); /* Execute command */
+			// Exit to avoid memory leak
+			exit(0);
+		}
+	}
+}
+
+/*void parseCommand(command) {
+	
+	if (command == "exit") {
+		exit(0);
+	}
+	if (command == "") {
+		
+	}
+}*/
