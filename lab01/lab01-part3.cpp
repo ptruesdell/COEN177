8d47f7dd3518ed9612f51ddf45c80defef878239
ptruesdell <parkertruesdell@gmail.com>
2016-10-04 17:00:34 -0700

    part three almost working

--- lab01/lab01-part3.cpp
+++ lab01/lab01-part3.cpp
@@ -28,6 +28,7 @@ void runFork(int n) {
 */
 
 pid_t pid;
+int childCount = 0;
 
 int main(int argc, char **argv) {
 
@@ -61,6 +62,74 @@ int main(int argc, char **argv) {
 						else if (pid == 0) {
 							/* Child code runs here */
 							cout << "Child PID: " << getpid() << endl;
+							for (int l = 0; l < 3; l++) {
+								pid = fork();
+								if (pid < 0) {
+									/* Error handling */
+									cout << "Failed to fork, exiting..." << endl;
+									return -1;
+								}
+								else if (pid == 0) {
+									/* Child code runs here */
+									cout << "Child PID: " << getpid() << endl;
+									while (childCount < 2) {
+										if (childCount == 0) {
+											// Generate 3 children for the first node on the 3rd level of the tree
+											childCount++;
+											for (int m = 0; m < 3; m++) {
+												pid = fork();
+
+												if (pid < 0) {
+													/* Error handling */
+													cout << "Failed to fork, exiting" << endl;
+													return -1;
+												}
+												else if (pid == 0) {
+													// Child code runs here
+													cout << "Child PID: " << getpid() << endl;
+													return 0;
+												}
+												else {
+													// Parent code runs here
+													waitpid( -1, NULL, 0);
+													if (m == 2) {
+														cout << "Parent PID: " << getpid() << endl;
+													}
+												}
+											}
+										}
+										else if (childCount == 1) {
+											// Generate 2 children for the second node on the 3rd level
+											childCount++;
+											for (int n = 0; n < 2; n++) {
+												pid = fork();
+
+												if (pid < 0) {
+													// Error handling
+													cout << "Failed to fork, exiting..." << endl;
+													return -1;
+												}
+												else if (pid == 0) {
+													// Child code runs here
+													cout << "Child PID: " << getpid() << endl;
+													return 0;
+												}
+												else {
+													// Parent code runs here
+													waitpid(-1, NULL, 0);
+													if (n == 1) {
+														cout << "Parent PID: " << getpid() << endl;
+													}
+												}
+											}
+										}
+										else {
+											return 0;
+										}
+
+								}
+								return 0;
+							}
 							return 0;
 						}
 						else {
@@ -71,6 +140,7 @@ int main(int argc, char **argv) {
 							}
 						}
 					}
+					return 0;
 				}
 				else {
 					/* Parent code runs here */
@@ -79,7 +149,9 @@ int main(int argc, char **argv) {
 						cout << "Parent PID: " << getpid() << endl;
 					}
 				}
-			}	
+			}
+			return 0;
+		}	
 		else {
 			/* Parent code runs here */
 			waitpid(-1, NULL, 0);
@@ -88,6 +160,4 @@ int main(int argc, char **argv) {
 			}
 		}
 	}
-		}
-}
 }
